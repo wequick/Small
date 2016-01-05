8db511d5cdbca1b766c619df7da387aed2d9b947
galenlin <oolgloo.2012@gmail.com>
2016-01-05 10:22:23 +0800

    Remove unused import

--- Android/aar-small/src/main/java/net/wequick/small/WebBundleLauncher.java
+++ Android/aar-small/src/main/java/net/wequick/small/WebBundleLauncher.java
@@ -28,13 +28,8 @@ import net.wequick.small.webkit.WebActivity;
 import net.wequick.small.webkit.WebViewPool;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
 
 /**
  * Created by galen on 15/1/28.
