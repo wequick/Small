72bdfab690bd5662c3f4f7c33168aac763dfaa49
galenlin <oolgloo.2012@gmail.com>
2016-01-11 15:02:06 +0800

    Add passing parameter example

--- Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/MainActivity.java
+++ Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/MainActivity.java
@@ -1,8 +1,12 @@
 package net.wequick.example.small.app.detail;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import net.wequick.small.Small;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Detail");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Uri uri = Small.getUri(this);
+        if (uri != null) {
+            String from = uri.getQueryParameter("from");
+            if (from != null) {
+                TextView tvFrom = (TextView) findViewById(R.id.tvFrom);
+                tvFrom.setText("-- Greet from " + from);
+            }
+        }
     }
 
     @Override
