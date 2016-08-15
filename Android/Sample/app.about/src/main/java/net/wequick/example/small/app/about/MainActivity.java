50d79faab55418fb2d0356ac5125120e37525b4f
galenlin <oolgloo.2012@gmail.com>
2016-08-15 11:00:09 +0800

    Add back button for app.about

--- Android/Sample/app.about/src/main/java/net/wequick/example/small/app/about/MainActivity.java
+++ Android/Sample/app.about/src/main/java/net/wequick/example/small/app/about/MainActivity.java
@@ -2,6 +2,7 @@ package net.wequick.example.small.app.about;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button btn = (Button) findViewById(R.id.btn_detail);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +40,14 @@ public class MainActivity extends AppCompatActivity {
                 Small.openUri("http://code.wequick.net", MainActivity.this);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
