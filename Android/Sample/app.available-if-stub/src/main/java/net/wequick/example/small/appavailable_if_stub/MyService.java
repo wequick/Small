363f4fb3d1ff8c7e6dd6d9dd1364343a8cb87e13
galenlin <oolgloo.2012@gmail.com>
2016-11-04 14:55:27 +0800

    Add files for stub sample

---
+++ Android/Sample/app.available-if-stub/src/main/java/net/wequick/example/small/appavailable_if_stub/MyService.java
@@ -0,0 +1,33 @@
+package net.wequick.example.small.appavailable_if_stub;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by galen on 2016/11/4.
+ */
+public class MyService extends Service {
+
+    private static final String TAG = "MyService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
