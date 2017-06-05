b4780b9a16fed4f7ae4e66801e1a39ac787ec2fe
galenlin <oolgloo.2012@gmail.com>
2017-06-05 23:14:38 +0800

    Support loading bundle class with Class.forName in host

--- Android/DevSample/small/src/main/java/net/wequick/small/ApkClassLoader.java
+++ Android/DevSample/small/src/main/java/net/wequick/small/ApkClassLoader.java
@@ -23,6 +23,7 @@ import net.wequick.small.util.ReflectAccelerator;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -42,8 +43,31 @@ class ApkClassLoader extends ClassLoader {
     private String[] mMergedAssetPaths;
     private ApkInstrumentation mInstrumentation;
 
-    ApkClassLoader(ClassLoader parent, ApkInstrumentation instrumentation) {
-        super(parent);
+    /**
+     * Create an apk class loader to load resource and class in multi apk bundles.
+     *
+     * This will resort the class loader link from:
+     *
+     *          host ->             boot
+     * to:
+     *
+     *  this -> host -> multiDex -> boot
+     *  ^^^^                              load resource and find library in apk bundles
+     *          ^^^^                      the original host class loader
+     *                  ^^^^^^^^          load dex in apk bundles
+     *                              ^^^^  the boot class loader
+     *
+     */
+    ApkClassLoader(ClassLoader host, ApkInstrumentation instrumentation)
+            throws NoSuchFieldException, IllegalAccessException {
+        super(host);
+
+        Field f = ClassLoader.class.getDeclaredField("parent");
+        f.setAccessible(true);
+        ClassLoader boot = host.getParent();
+        MultiDexClassLoader multiDex = new MultiDexClassLoader(boot);
+        f.set(host, multiDex);
+
         mInstrumentation = instrumentation;
     }
 
@@ -111,43 +135,6 @@ class ApkClassLoader extends ClassLoader {
                 }
             }
         }, "net.wequick.small.apk.preload").start();
-    }
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        Class<?> clazz;
-
-        if (mApks == null) return super.findClass(name);
-
-        // Find class in loaded bundles
-        for (ApkElement bundle : mApks) {
-            if (bundle.dexFile != null) {
-                clazz = bundle.dexFile.loadClass(name, this);
-                if (clazz != null) {
-                    return clazz;
-                }
-            }
-        }
-
-        // Find class in lazy-load bundles
-        for (ApkElement apk : mApks) {
-            if (apk.dexFile != null) continue;
-            // FIXME: Check if the class is in a apk
-            // As now, we simply check if the class name is starts with the apk package name,
-            // but there are cases that classes from multi-package are compiled into one apk.
-            boolean isInBundle = name.startsWith(apk.packageName);
-            if (!isInBundle) continue;
-
-            DexFile dexFile = loadApkLocked(apk);
-            if (dexFile != null) {
-                clazz = dexFile.loadClass(name, this);
-                if (clazz != null) {
-                    return clazz;
-                }
-            }
-        }
-
-        return super.findClass(name);
     }
 
     private void loadApk(ApkElement apk) {
@@ -243,7 +230,7 @@ class ApkClassLoader extends ClassLoader {
         if (clazz == null) return;
 
         try {
-            final Class applicationClass = findClass(clazz);
+            final Class applicationClass = loadClass(clazz);
             Bundle.postUI(new Runnable() {
                 @Override
                 public void run() {
@@ -260,5 +247,49 @@ class ApkClassLoader extends ClassLoader {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Failed to create application: " + clazz, e);
         }
+    }
+
+    private class MultiDexClassLoader extends ClassLoader {
+        MultiDexClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            Class<?> clazz;
+
+            if (mApks == null) return null;
+
+            // Find class in loaded bundles
+            for (ApkElement bundle : mApks) {
+                if (bundle.dexFile != null) {
+                    clazz = bundle.dexFile.loadClass(name, ApkClassLoader.this);
+                    if (clazz != null) {
+                        return clazz;
+                    }
+                }
+            }
+
+            // Find class in lazy-load bundles
+            for (ApkElement apk : mApks) {
+                if (apk.dexFile != null) continue;
+                // FIXME: Check if the class is in a apk
+                // As now, we simply check if the class name is starts with the apk package name,
+                // but there are cases that classes from multi-package are compiled into one apk.
+                boolean isInBundle = name.startsWith(apk.packageName);
+                if (!isInBundle) continue;
+
+                DexFile dexFile = loadApkLocked(apk);
+                if (dexFile != null) {
+                    clazz = dexFile.loadClass(name, ApkClassLoader.this);
+                    if (clazz != null) {
+                        return clazz;
+                    }
+                }
+            }
+
+            return null;
+        }
+
     }
 }
