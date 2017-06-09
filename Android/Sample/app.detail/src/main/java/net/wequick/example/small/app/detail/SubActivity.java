c3b3eb2b94415492a2aa3d04a71d568dcf7692f8
galenlin <oolgloo.2012@gmail.com>
2017-06-10 01:45:33 +0800

    Add databinding sample in multi modules

--- Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/SubActivity.java
+++ Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/SubActivity.java
@@ -5,7 +5,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
-//import net.wequick.example.small.app.detail.databinding.ActivitySubBinding;
+import net.wequick.example.small.app.detail.databinding.ActivitySubBinding;
 
 /**
  * Created by galen on 16/4/11.
@@ -16,8 +16,8 @@ public class SubActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        ActivitySubBinding binding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_sub, null,false);
-//        setContentView(binding.getRoot());
-//        binding.setHandlers(new MyHandler());
+        ActivitySubBinding binding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_sub, null,false);
+        setContentView(binding.getRoot());
+        binding.setHandlers(new MyHandler());
     }
 }
